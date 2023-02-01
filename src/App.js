4ef831a41a2f3e7468d0e1977130275a7c8b0073
Abhishek Jain <bhijin@amazon.com>
2023-02-01 11:54:39 -0800

    Video 10 completed, app to enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 
 function  App() {
   return (
     <>
     <Navbar title="Text Utils Page" aboutText="aboutTextUtils"/>
-    <div className="container my-3">
+    {/* <div className="container my-3">
       <TextForm heading="Enter text to analyse"/>
-    </div>    
+    </div>     */}
+    <div className="container my-3">
+      <About/>
+    </div>  
     </>    
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,10 +5,22 @@ import React, {useState} from 'react'
 export default function TextForm(props) {
     const [myText, setText] = useState("Default Text of use state"); 
 
-    const handleOnClick = () => {
+    const uppercaseButtonHandleOnClick = () => {
         let newText = myText.toUpperCase();
         setText(newText);
         console.log("Upper case button clicked");
+    }
+
+    const lowercaseButtonHandleOnClick = () => {
+        let newText = myText.toLowerCase();
+        setText(newText);
+        console.log("Lower case button clicked");
+    }
+
+    const clearTextButtonHandleOnClick = () => {
+        let newText = '';
+        setText(newText);
+        console.log("Clear text button clicked");
     }
 
     const handleOnChange = (event) => {
@@ -29,7 +41,31 @@ export default function TextForm(props) {
         <label htmlFor="mybox" className="form-label">Enter your Text below</label>
         <textarea className="form-control" value={myText} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
 
-        <button className={`btn btn-primary my-3`} onClick={handleOnClick} >Covert to upper case</button>
+        <button className={`btn btn-primary my-3`} onClick={uppercaseButtonHandleOnClick} >Covert to upper case</button>
+        <button className={`btn btn-primary mx-1`} onClick={lowercaseButtonHandleOnClick} >Covert to lower case</button>
+        <button className={`btn btn-primary mx-1`} onClick={clearTextButtonHandleOnClick} >Covert to clear text</button>
+    </div>
+
+    <div className="container">
+        <p>
+            Text has {myText.split(' ').length} words and {myText.length} characters
+        </p>
+    </div>
+
+    <div className="container">
+        <p>
+            Text will take {myText.length*0.008} minutes to read the text
+        </p>
+    </div>
+
+
+    <div className="container">
+        <h2>
+            Preview of the Text
+        </h2>
+        <p>
+            {myText}
+        </p>
     </div>
  
     </>
@@ -40,3 +76,4 @@ export default function TextForm(props) {
 TextForm.prototype = {
     heading : String
 }
+
